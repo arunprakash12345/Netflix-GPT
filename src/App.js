d6742455edd1885fab59c915b02284991fae1e29
arunprakash12345 <arunprakash74119@gmail.com>
2026-04-04 13:09:28 +0530

    Configuration done.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+function App() {
+  return (
+    <div className="text-3xl font-bold underline text-red-500">
+      Namaste everyone, lets build Netflix GPT together!
+    </div>
+  );
+}
+
+export default App;
